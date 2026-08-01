c66554a8fd4603817cc7203a2edeb7eb8650d3a4
Balbir Singh <balbirs2204@gmail.com>
2026-08-02 01:13:53 +0530

    chore: remove legacy database credentials

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  mongoose
-    .connect("mongodb+srv://balbirs2204:mypassword@cluster0.v3u9con.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
-    });
+export const dbConnection = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not configured");
+  }
+
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to database!");
 };
